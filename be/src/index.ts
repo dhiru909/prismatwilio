c2c9e51fce42a1faaaed8243450d8591f2aad8dc
dhiru909 <dhirajkhali1@gmail.com>
2025-01-19 19:47:15 +0530

    add docker

--- be/src/index.ts
+++ be/src/index.ts
@@ -1,9 +1,12 @@
 import express from "express";
 import userRouter from "./router/user";
 import errorHandler from "./utils/errorHandler";
+import dotenv from 'dotenv'
+
+dotenv.config()
 const app = express();
 app.use(express.json());
 app.use(errorHandler);
-app.use("/api/v1/user",userRouter);
+app.use("/api/v1/user", userRouter);
 app.listen(3000);
 console.log("listening on port 3000");
